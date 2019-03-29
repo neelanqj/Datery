2715e27cd63389c6d4897093924217cf306632a3
Neelan Joachimpillai <neelan.joachimpillai@gmail.com>
2019-03-28 20:52:14 -0500

    Implemented Like functionality on API layer and SPA layer.

--- Datery.API/Data/DatingRepository.cs
+++ Datery.API/Data/DatingRepository.cs
@@ -62,9 +62,20 @@ namespace Datery.API.Data
                 .OrderByDescending(u => u.LastActive)
                 .AsQueryable();
             users = users.Where(u => u.Id != userParams.UserId && u.Gender == userParams.Gender);
-            
 
-            if(userParams.MinAge != 18 || userParams.MaxAge != 99)
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                 var minDob = DateTime.Today.AddYears(-userParams.MaxAge -1);
                 var maxDob = DateTime.Today.AddYears(-userParams.MinAge -1);
@@ -85,6 +96,25 @@ namespace Datery.API.Data
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(x => x.Likees)
+                .Include(x => x.Likers)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id)
+                    .Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likees.Where(u => u.LikerId == id)
+                    .Select(i => i.LikeeId);
+            }
         }
 
         public async Task<bool> SaveAll()
