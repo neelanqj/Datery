e5d87ef0865e5e1862c0edc0ed3090e5879c2842
Neelan Joachimpillai <neelan.joachimpillai@gmail.com>
2019-04-17 23:25:25 -0500

    unread functionality (must be refined, is partly buggy)

--- Datery-SPA/src/app/members/member-messages/member-messages.component.ts
+++ Datery-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '../../_models/message';
 import { UserService } from 'src/app/_services/user.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -22,10 +23,19 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages(){
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(
       this.authService.decodedToken.nameid,
       this.recipientId
-    ).subscribe(messages => {
+    ).pipe(tap(messages => {
+      for(let i = 0; i < messages.length; i++) {
+        if (messages[i].isRead === false
+          && messages[i].recipientId === currentUserId){
+            this.userService.markAsRead(currentUserId, messages[i].id);
+          }
+      }
+    }))
+    .subscribe(messages => {
       this.messages = messages;
     }, error=>{
       this.alertify.error(error);
